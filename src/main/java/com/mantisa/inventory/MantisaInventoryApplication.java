7873f8ea4e769190c3e5155a3e58204a064ea5e4
Riquelmi Palacios <90204956+riquelmip@users.noreply.github.com>
2024-10-21 09:48:23 -0600

    Update MantisaInventoryApplication.java

--- src/main/java/com/mantisa/inventory/MantisaInventoryApplication.java
+++ src/main/java/com/mantisa/inventory/MantisaInventoryApplication.java
@@ -3,15 +3,27 @@ package com.mantisa.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class MantisaInventoryApplication {
 
     public static void main(String[] args) {
-        // Cargar el perfil basado en una variable de entorno
+        System.out.println("SPRING_PROFILES_ACTIVE: " + System.getenv("SPRING_PROFILES_ACTIVE"));
+        System.out.println("SPRING_DATASOURCE_HOST: " + System.getenv("SPRING_DATASOURCE_HOST"));
+        System.out.println("SPRING_DATASOURCE_PORT: " + System.getenv("SPRING_DATASOURCE_PORT"));
+        System.out.println("SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+
+
         String profile = System.getenv("SPRING_PROFILES_ACTIVE");
         if (profile != null && !profile.isEmpty()) {
+            System.out.println("Profile active: " + profile);
             System.setProperty("spring.profiles.active", profile);
+        } else {
+            System.out.println("No profile active, using default.");
         }
+
         SpringApplication.run(MantisaInventoryApplication.class, args);
     }
 
