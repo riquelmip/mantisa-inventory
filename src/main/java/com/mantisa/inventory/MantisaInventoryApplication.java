9eb629a64594abe9310dfe01c233f7856a881565
Riquelmi Palacios <90204956+riquelmip@users.noreply.github.com>
2024-10-21 10:44:18 -0600

    pruebas

--- src/main/java/com/mantisa/inventory/MantisaInventoryApplication.java
+++ src/main/java/com/mantisa/inventory/MantisaInventoryApplication.java
@@ -1,6 +1,5 @@
 package com.mantisa.inventory;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MantisaInventoryApplication {
 
     public static void main(String[] args) {
-        // Cargar las variables de entorno desde el archivo .env
-        Dotenv dotenv = Dotenv.load();
-
-        // Se pueden usar las variables, pero no es necesario si no se usan
-        String host = dotenv.get("SPRING_DATASOURCE_HOST");
-        String port = dotenv.get("SPRING_DATASOURCE_PORT");
-
-        System.out.println("Database host: " + host);
-        System.out.println("Database port: " + port);
-
         SpringApplication.run(MantisaInventoryApplication.class, args);
     }
 }
